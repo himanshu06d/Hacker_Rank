a283df26c11f8e170fd099849dc3a462a6caff28
Himanshu Dubey <hd156681@gmail.com>
2021-03-15 14:10:35 +0530

    error resolved

--- C++/variable_sized_array.cpp
+++ C++/variable_sized_array.cpp
@@ -1,6 +1,7 @@
 /*The first line contains two space-separated integers denoting the respective values of  (the number of variable-length arrays) and  (the number of queries).
 Each line  of the  subsequent lines contains a space-separated sequence in the format k a[i]0 a[i]1 … a[i]k-1 describing the -element array located at .
 Each of the  subsequent lines contains two space-separated integers describing the respective values of  (an index in array ) and  (an index in the array referenced by ) for a query.*?*/
+//code
 #include <cmath>
 #include <cstdio>
 #include <vector>
