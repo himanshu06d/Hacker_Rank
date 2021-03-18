b1785d3b20e7c806f3fa1da7778d09bf1ad0d742
Himanshu Dubey <hd156681@gmail.com>
2021-03-18 23:54:23 +0530

    java static initializer block

---
+++ java/problem_10.java
@@ -0,0 +1,12 @@
+static{
+    try{
+        if(B <= 0 || H <= 0){
+            flag = false;
+            throw new Exception("Breadth and height must be positive");
+        }
+    }catch(Exception e){
+        System.out.println(e);
+    }
+
+}
+
