cde49dafff2fc6a51e7fcd9c67ea179b5a23c4c2
Himanshu Dubey <hd156681@gmail.com>
2021-03-19 22:52:25 +0530

    string input and priting using printf

--- java/problem_5.java
+++ java/problem_5.java
@@ -6,10 +6,10 @@ public class Solution {
             Scanner sc=new Scanner(System.in);
             System.out.println("================================");
             for(int i=0;i<3;i++){
-                String s1=sc.next();
+                String s1=sc.next(); //taking string aas a input
                 int x=sc.nextInt();
                 //Complete this line
-                System.out.printf("%-15s%03d%n", s1, x);
+                System.out.printf("%-15s%03d%n", s1, x); //format specifier
             }
             System.out.println("================================");
 
