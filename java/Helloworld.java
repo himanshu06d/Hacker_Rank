41cecdc4c32a066b6d22a6bbb4218b3793682875
Himanshu Dubey <hd156681@gmail.com>
2021-03-19 22:37:24 +0530

    print hello world

--- java/Helloworld.java
+++ java/Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello world");
+System.out.println("Hello world"); //print hello world and change the line
 	}
 
 }
